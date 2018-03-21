0427f0b6a934890823c5af6bb25cd7de48097bf1
Juna <dev.jj@icloud.com>
2018-03-21 12:52:31 +0900

    コメント機能の実装(6.4)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,15 @@
 class CommentsController < ApplicationController
+  def create
+#    記事(のオブジェクト)を取得して@articleに保存
+    @article = Article.find(params[:article_id])
+#    コメントの作成と保存
+    @comment = @article.comments.create(comment_params)
+#    元の記事の画面にリダイレクト
+    redirect_to article_path(@article)
+  end
+ 
+    private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
 end
