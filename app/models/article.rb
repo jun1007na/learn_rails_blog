493c30aa1e1efb84d0b7bc41dce29d39c8706fbd
Juna <dev.jj@icloud.com>
2018-03-22 11:08:49 +0900

    コメントの削除機能(8)

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
-#  多数のコメントを関連付け
-  has_many :comments
+#  多数のコメントを関連付け, 記事に関連付けられているコメントを削除
+  has_many :comments, dependent: :destroy
 #  すべての記事にタイトルが存在し、その長さが5文字以上であるバリデーション
   validates :title, presence: true, length: {minimum: 5}
 end
